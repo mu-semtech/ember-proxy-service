013dc4e7c589e25810eede3597db194700af0903
Claire Lovisa <lovisa.claire@gmail.com>
2020-04-18 12:03:32 +0200

    Map environment variables to index.html of frontend

--- ember-proxy-service.sh
+++ ember-proxy-service.sh
@@ -4,4 +4,14 @@ REGEX=$(echo "$STATIC_FOLDERS_REGEX" | sed -e 's/[\/&]/\\&/g')
 
 sed -i "s/STATIC_FOLDERS_REGEX/$REGEX/" /etc/nginx/conf.d/app.conf
 
+# Replace environment variables in the build of the frontend
+PREFIX="CONFIG_"
+ENV_VARIABLES=$(env | grep "${PREFIX}")
+
+while IFS= read -r line; do
+  ENV_VARIABLE=$(echo "$line" | sed -e "s/^$PREFIX//" | cut -f1 -d"=")
+  VALUE=$(echo "$line" | cut -d"=" -f2-)
+  sed -i "s/%7B%7B$ENV_VARIABLE%7D%7D/$VALUE/g" /app/index.html
+done <<< "$ENV_VARIABLES"
+
 nginx -g "daemon off;"
